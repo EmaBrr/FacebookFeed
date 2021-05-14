37512728ff60034b87147acf4a4d15ede883d659
EmaBrr <emabartasyte@gmail.com>
2021-05-14 16:56:41 +0300

    Javascript taking elements from array

--- js/main.js
+++ js/main.js
@@ -3,28 +3,55 @@
 function getData(data) {
 
     let HTML = '';
-    let sectionBody = document.querySelector('.middle__part')
+    let cardBody = document.querySelector('.middle__part');
 
     if (Array.isArray(data)) {
-        for (let i = 0; i < data.length; i++){
 
-            HTML += getList(data[i]);
+        for (let i = 0; i < data.length; i++ ){
+
+            HTML+= getList (data[i]);
         }
-        return sectionBody.innerHTML = ''
+
+        return cardBody.innerHTML = HTML;
+    } else {
+        return console.log('Tai ne masyvas!')
     }
-    else {
-        return console.log('Tai ne masyvas')
-    }
-    
 }
 
-function getList () {
+function getList(list) {
 
-    let HTML = `
-    `;
+    let HTML = `<div class="card">
+                    <div class="card__header">
+                        <div class="img"><img src="./img/user.jpg" alt=""></div>
+                        <div class="name__surname">
+                            <p>${list.autorius.vardas} ${list.autorius.pavarde}</p>
+                            <p class="date">${list.laikas}</p>
+                        </div>
+                        <div class="more"><p>...</p></div>
+                    </div>
+                    <div class="card__content">
+                        <div class="content">${list.pranesimas.tekstas}</div>
+                    </div>
+                    <div class="card__footer">
+                        <div class="card__footer1">
+                            <div class="like__button"><button type="submit"><i class="far fa-thumbs-up"></i>Like</button></div>
+                            <div class="like__button"><button type="submit"><i class="far fa-comments"></i>Comment</button></div>
+                            <div class="like__button"><button type="submit"><i class="far fa-share-square"></i>Share</button></div>
+                        </div>
+                        <div class="card__footer2">
+                            <div class="img"><img src="./img/user.jpg" alt=""></div>
+                            <textarea name="Comment" id="" cols="30" rows="5" placeholder="Write your comment"></textarea>
+                            <div class="emoji__row">
+                                <i class="far fa-smile"></i>
+                                <i class="fas fa-camera"></i>
+                                <i class="far fa-image"></i>
+                                <i class="far fa-share-square"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
 
     return HTML;
-
 }
 
 getData(feed);
