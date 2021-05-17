17f187af65575137bb2c1f01da641e3dc5ee84f0
EmaBrr <emabartasyte@gmail.com>
2021-05-17 12:24:10 +0300

    deleted comment

--- js/main.js
+++ js/main.js
@@ -81,17 +81,6 @@ function timeDifference(time) {
   }
 }
 
-// function timeDifference(s) {
-//     var ms = s % 1000;
-//     s = (s - ms) / 1000;
-//     var secs = s % 60;
-//     s = (s - secs) / 60;
-//     var mins = s % 60;
-//     var hrs = (s - mins) / 60;
-  
-//     return hrs + ':' + mins + ':' + secs + '.' + ms;
-//   }
-
 function getGalery(arrayGalery) {
   if (arrayGalery === 0) {
     return "";
